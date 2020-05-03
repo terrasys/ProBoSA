717458cecf83f4571adeed0195bb8773b7648bd9
Markus Möller <markus.moeller@terrasys.info>
2020-05-03 17:16:02 +0200

    Delete BAG_lu_f.R

--- Regeln/BAG_lu_f.R
+++
@@ -1,5 +0,0 @@
-s$BAG_lu_f <- ifelse((s$f_koe_t>=8  & s$f_koe_t<12  & s$f_koe_u>=65 & s$f_koe_u<92) | 
-                           (s$f_koe_t>=12 & s$f_koe_t<17  & s$f_koe_u>=65 & s$f_koe_u<88) | 
-                           (s$f_koe_t>=8  & s$f_koe_t<17  & s$f_koe_u>=50 & s$f_koe_u<65),
-                       w.f,
-                       0)
