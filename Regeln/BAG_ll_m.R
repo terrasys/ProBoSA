74156d3ad60f568a8cd2c126bc878645758c93d4
Markus Möller <markus.moeller@terrasys.info>
2020-05-03 17:14:08 +0200

    Delete BAG_ll_m.R

--- Regeln/BAG_ll_m.R
+++
@@ -1,6 +0,0 @@
-s$BAG_ll_m <- ifelse((s$m_koe_t>=17 & s$m_koe_t<25 & s$m_koe_u>=40 & s$m_koe_u<50) | 
-                           (s$m_koe_t>=17 & s$m_koe_t<25 & s$m_koe_u>=30 & s$m_koe_u<40) | 
-                           (s$m_koe_t>=17 & s$m_koe_t<25 & s$m_koe_u>=15 & s$m_koe_u<30) |
-                           (s$m_koe_t>=25 & s$m_koe_t<35 & s$m_koe_u>=30 & s$m_koe_u<50),
-                       w.m,
-                       0)
