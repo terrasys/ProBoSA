fc39e1d7e9f2a8fed48a4aa6419826ef2efb696f
Markus Möller <markus.moeller@terrasys.info>
2020-05-03 17:32:55 +0200

    Delete BAG_tu_bs.R

--- Regeln/BAG_tu_bs.R
+++
@@ -1,4 +0,0 @@
-s$BAG_tu_bs <- ifelse((s$bs_koe_t>=17  & s$bs_koe_t<25  & s$bs_koe_u>=65 & s$bs_koe_u<83) | 
-                      (s$bs_koe_t>=17  & s$bs_koe_t<30  & s$bs_koe_u>=50 & s$bs_koe_u<65),
-                       w.bs,
-                       0)
