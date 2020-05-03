4a4d40d261aadd803d5502433821a00513f86c55
Markus Möller <markus.moeller@terrasys.info>
2020-05-03 17:18:31 +0200

    Delete BAG_lu_v.R

--- Regeln/BAG_lu_v.R
+++
@@ -1,5 +0,0 @@
-s$BAG_lu_v <- ifelse((s$v_koe_t>=8  & s$v_koe_t<12  & s$v_koe_u>=65 & s$v_koe_u<92) | 
-                           (s$v_koe_t>=12 & s$v_koe_t<17  & s$v_koe_u>=65 & s$v_koe_u<88) | 
-                           (s$v_koe_t>=8  & s$v_koe_t<17  & s$v_koe_u>=50 & s$v_koe_u<65),
-                       w.v,
-                       0)
