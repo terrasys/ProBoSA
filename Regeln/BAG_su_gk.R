43509f66d8970414983ecd9bdd9799b9d255aaa8
Markus Möller <markus.moeller@terrasys.info>
2020-05-03 17:32:32 +0200

    Delete BAG_su_gk.R

--- Regeln/BAG_su_gk.R
+++
@@ -1,4 +0,0 @@
-s$BAG_su_gk <- ifelse((s$gk_koe_t>0 & s$gk_koe_t<8 & s$gk_koe_u>=80 & s$gk_koe_u<=100) | 
-                      (s$gk_koe_t>0 & s$gk_koe_t<8 & s$gk_koe_u>=50 & s$gk_koe_u<80),
-                       w.gk,
-                       0)
