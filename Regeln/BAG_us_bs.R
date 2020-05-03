cb53498920e4655753c92833737f5a7fbd6b8cf3
Markus Möller <markus.moeller@terrasys.info>
2020-05-03 20:10:44 +0200

    Delete BAG_us_bs.R

--- Regeln/BAG_us_bs.R
+++
@@ -1,4 +0,0 @@
-s$BAG_us_bs <- ifelse((s$bs_koe_t>0 & s$bs_koe_t<8 & s$bs_koe_u>=25 & s$bs_koe_u<40) | 
-                      (s$bs_koe_t>0 & s$bs_koe_t<8  & s$bs_koe_u>=40 & s$bs_koe_u<50),
-                       w.bs,
-                       0)
