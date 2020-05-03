64ae08b19d32a3790a2f0c80a7062ef37337563f
Markus Möller <markus.moeller@terrasys.info>
2020-05-03 20:11:46 +0200

    Delete BAG_ut_m.R

--- Regeln/BAG_ut_m.R
+++
@@ -1,5 +0,0 @@
-s$BAG_ut_m <- ifelse((s$m_koe_t>=35 & s$m_koe_t<45  & s$m_koe_u>=30 & s$m_koe_u<50) | 
-                           (s$m_koe_t>=30 & s$m_koe_t<45  & s$m_koe_u>=50 & s$m_koe_u<65) | 
-                           (s$m_koe_t>=25 & s$m_koe_t<35  & s$m_koe_u>=65 & s$m_koe_u<75),
-                       w.m,
-                       0)
